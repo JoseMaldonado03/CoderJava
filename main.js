1b33acf31f5187442d86f34f4ed339acb3c48092
Jose Maldonado <muso030397@gmail.com>
2024-12-19 20:23:30 -0300

    entrega casi final

--- main.js
+++ main.js
@@ -1,174 +1,171 @@
-let productos_disponibles = [
-  {
-    id: 1,
-    name: "Poseidon",
-    img: "assets/Cuadro1.jpg",
-    elemento: "Pintura",
-    precio: 25000,
-  },
-  {
-    id: 2,
-    name: "Virgen del velo",
-    img: "assets/Cuadro2.jpg",
-    elemento: "Pintura",
-    precio: 25000,
-  },
-  {
-    id: 3,
-    name: "Geometric Queen",
-    img: "assets/Cuadro3.jpg",
-    elemento: "Pintura",
-    precio: 35000,
-  },
-  {
-    id: 4,
-    name: "Vilipendia",
-    img: "assets/Cuadro4.jpg",
-    elemento: "Pintura",
-    precio: 20000,
-  },
-];
+document.getElementById("carrito")
+    .addEventListener("click", () => {
+        document.getElementById("carritoDeCompra")
+            .classList.toggle("activar");
+    })
+document.getElementById("boton-cerrar")
+    .addEventListener("click", () => {
+        document.getElementById("carritoDeCompra")
+            .classList.toggle("activar");
+    })
+
+const solicitarInfo = async () => {
+    console.log("solicitarInfo")
+    const response = await fetch("./data.json")
+    const arrayProducto = await response.json()
+    return arrayProducto
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const productos_disponibles = await solicitarInfo()
+    productos_disponibles.forEach((p) => {
+        document.getElementById("productos")
+            .innerHTML +=
+            `<p id="${p.id}" class="produto-item">
+        ${p.name} 
+        <div class="img">
+          <img src="${p.img}" alt="${p.name}" />
+        </div> 
+        - ARS ${p.precio}
+      </p>
+      `;
+    });
+
+    let produtos_seleccionados = JSON.parse(localStorage.getItem("produtos_seleccionados")) || [];
 
 
-productos_disponibles.forEach((p) => {
-  document.getElementById("productos").innerHTML += 
-  `<p id="${p.id}" class="produto-item">
-      ${p.name} 
-      <div class="img">
-        <img src="${p.img}" alt="${p.name}" />
-      </div> 
-      - ARS ${p.precio}
-    </p>
-    `
-    ;
-});
-  
-let produtos_seleccionados = [];
+    let produtos = document.querySelectorAll(".productos li")
 
-document.getElementById("carrito").addEventListener("click", () => {
-  document.getElementById("carritoDeCompra").classList.toggle("activar");
-})
+    document.body.addEventListener("click", (event) => {
 
-let produtos = document.querySelectorAll(".productos li")
+        if (event.target.className == "produto-item") {
+            let productoExists = produtos_seleccionados.findIndex((p) => p.id == event.target.id);
+            let productoSelecionado = productos_disponibles.filter((p) => p.id == event.target.id);
 
-document.body.addEventListener("click", (event) => {
-    if (event.target.className == "produto-item") {
-      let productoExists = produtos_seleccionados.findIndex((p) => p.id == event.target.id);
-      let productoSelecionado = productos_disponibles.filter((p) => p.id == event.target.id);
-  
-      if (productoExists < 0) {
-         produtos_seleccionados.push({ ...productoSelecionado[0], quantidade: 1 });
+            Swal.fire({
+                icon: "success",
+                title: "Usted agrego al carrito",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                toast: true,
+                position: "bottom-start"
+            })
+
+            if (productoExists < 0) {
+                produtos_seleccionados.push({
+                    ...productoSelecionado[0],
+                    quantidade: 1
+                });
+
+                return showListSelecionadosProdutos(produtos_seleccionados);
+            }
+
+
+            produtos_seleccionados[productoExists].quantidade += 1;
+
+
+            showListSelecionadosProdutos(produtos_seleccionados);
+
+        }
+    })
+
+    document.body.addEventListener("click", (event) => {
+        if (event.target.className == "producto_borrar") {
+            let productoExists = produtos_seleccionados.findIndex((p) => p.id == event.target.id);
+            if (productoExists >= 0 && produtos_seleccionados[productoExists].quantidade == 1) {
+                produtos_seleccionados = produtos_seleccionados.filter((p) => p.id != event.target.id);
+
+                return showListSelecionadosProdutos(produtos_seleccionados);
+            }
+
+            produtos_seleccionados[productoExists].quantidade -= 1;
+
+            showListSelecionadosProdutos(produtos_seleccionados);
+
+        }
+    });
+
+    const BotonComprar = document.getElementById("botonComprar");
+
+    BotonComprar.addEventListener("click", () => {
+        if (produtos_seleccionados.length === 0) {
+            Swal.fire({
+                icon: "error",
+                title: "No hay productos en el carrito",
+            });
+            return;
+        }
+
+        const total = produtos_seleccionados
+            .reduce((acc, p) => acc + p.precio * p.quantidade, 0)
+            .toFixed(2);
+
+        if (total == 0) {
+            return;
+        }
+
+        Swal.fire({
+                title: "Su total es de: " + total,
+                text: "¿Quiere continuar con la compra?",
+                showCancelButton: true,
+            })
+            .then((results) => {
+                if (results.isConfirmed) {
+                    Swal.fire({
+                            title: "Indique su email",
+                            input: "email",
+                        })
+                        .then((results) => {
+                            if (results.value) {
+                                produtos_seleccionados = [];
+                                localStorage.setItem("produtos_seleccionados", JSON.stringify(produtos_seleccionados));
+                                showListSelecionadosProdutos(produtos_seleccionados);
+                                Swal.fire({
+                                    title: "Compra realizada",
+                                    text: "¡Gracias por su compra!",
+                                });
+                            }
+                        });
+                } else {
+                    Swal.fire({
+                        title: "No pasa nada, puedes seguir comprando",
+                        timer: 1500,
+                        timerProgressBar: true,
+                    });
+                }
+            });
+    });
+
+    document.body.addEventListener("click", (event) => {
+        if (event.target.className == "producto_sumar") {
+            let productoExists = produtos_seleccionados.findIndex((p) => p.id == event.target.id);
+            if (productoExists >= 0 && produtos_seleccionados[productoExists].quantidade == -1) {
+                produtos_seleccionados = produtos_seleccionados.filter((p) => p.id != event.target.id);
+
+                return showListSelecionadosProdutos(produtos_seleccionados);
+            }
+
+            produtos_seleccionados[productoExists].quantidade += 1;
+
+            showListSelecionadosProdutos(produtos_seleccionados);
+
+        }
+    });
+
+    function showListSelecionadosProdutos(produtos) {
+        document.getElementById("productos-selecionados")
+            .innerHTML = "";
+
+        produtos.forEach((p) => {
+            document.getElementById("productos-selecionados")
+                .innerHTML += `
+        <li>
+          ${p.quantidade}x <strong>${p.name}</strong> <img src="${p.img}" alt="${p.name}" /> - ARS ${p.precio} unit / Total: ARS ${p.quantidade * p.precio} 
+       <button id = "${p.id}" class="producto_sumar">+</button> - <button id = "${p.id}" class="producto_borrar">-</button> </li> 
         
-      
-      return showListSelecionadosProdutos(produtos_seleccionados);
-      }
-  
-      produtos_seleccionados[productoExists].quantidade += 1;
-      
-      showListSelecionadosProdutos(produtos_seleccionados);
-}
-  });
+      `;
+        });
 
-function showListSelecionadosProdutos(produtos) {
-  document.getElementById("productos-selecionados").innerHTML = "";
-  
-  produtos.forEach((p) => {
-    document.getElementById("productos-selecionados").innerHTML += `
-      <li>
-        ${p.quantidade}x <strong>${p.name}</strong> - ARS ${p.precio} unit / Total: ARS ${p.quantidade * p.precio} 
-      </li>
-      
-    `;
-  });
- 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let peso = parseInt(prompt("¿Cuánto pesa?"))
-// let altura = Number(prompt("¿Cuánto mide"))
-
-// console.log("usted pesa " + peso + " usted mide " + altura)
-
-// let celcius = Number(prompt("Inguese la temperatura\nen grados celcius: "))
-
-// let fahrenheit = celcius * (9/5) + 32
-
-// alert ("la temperatura en fahrenheit= " + fahrenheit)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Alumnos = [{
-//     nombre: "sergio",
-//     dni:
-//     amteria []
-// }]
-
-// function agregadorDeAlumno(nombre,dni){  
-//     Alumnos.push{{
-//         nombre,
-//         dni,
-//         materia []
-//     }}
-
-// }
-
-// function mostradorDeAlunos(){
-//     for( let i =0; i < Alumnos.length; i++){
-//         constalumno = Alumnos [i];
-//         alumnos = alumnos + "\n" + alumno.nombre +"\n"
-//     } 
-//     alert("los alumnos en nuestra base son: " + alumnos)
-// }
-
-// function core () {
-//     let bandera = true
-//     let opcion = Number(prompt("Bienvenido a coderhose, que quiere hacer:\n 1-Agregar\n 2-eliminar\n 3=mostrar"))
-    
-//     if(opcion === 0){
-//         return
-//     }
-    
-//     while(bandera){
-//         switch (Option){
-//             case 1:
-//                 bandera = confirm("¿quiere seguir operando?")
-//                 break;
-//             case 2:
-//                 bandera = confirm("¿quiere seguir operando?")
-//                 break;
-//             case 3:
-//                 bandera = confirm("¿quiere seguir operando?")
-//                 break;
-//             default
-//                 alert("no existe")
-//                 bandera = confirm("¿quiere seguir operando?")
-//                 break;
-
-//         }    
-//     }
-// }
+    }
+})
